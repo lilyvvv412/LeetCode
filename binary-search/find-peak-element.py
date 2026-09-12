4cfdd748ef6fad136bf157b8178941778491fef5
Lily Luo <lilyvvv412@gmail.com>
2026-09-11 23:38:29 -0500

    LC-find-peak-element [Runtime: N/A; Memory: N/A]

---
+++ binary-search/find-peak-element.py
@@ -0,0 +1,11 @@
+#[0,n-2] (-1,n-1)
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left=-1
+        right=len(nums)-1
+        while left+1<right:
+            mid=(left+right)//2
+            if nums[mid]>nums[mid+1]:
+                right=mid
+            else: left=mid
+        return right
